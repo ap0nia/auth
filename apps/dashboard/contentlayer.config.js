5509e788bb63f5a390087a2418b363c18fbb6705
Aponia <brianmv@uci.edu>
2024-04-29 07:08:22 -0700

    chore: 🔧 disable contentlayer warning

--- apps/dashboard/contentlayer.config.js
+++ apps/dashboard/contentlayer.config.js
@@ -5,6 +5,7 @@ import { makeSource } from 'contentlayer/source-files'
 const config = makeSource({
   contentDirPath: './src/content',
   documentTypes: [],
+  disableImportAliasWarning: true,
 })
 
 export default config
